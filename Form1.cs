3cd21a656830f22bfc66102a8ce85de2a6e635c8
Madhu <madhuhack01@gmail.com>
2026-04-08 15:43:47 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VulnThickClient1
+{
+    public class Form1 : Form
+    {
+        TextBox txtUser;
+        TextBox txtPass;
+        Button btnLogin;
+        Label lblResult;
+
+        public Form1()
+        {
+            this.Text = "Login";
+            this.Size = new Size(400, 250);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Username
+            txtUser = new TextBox();
+            txtUser.Location = new Point(120, 40);
+            txtUser.Width = 150;
+
+            // Password
+            txtPass = new TextBox();
+            txtPass.Location = new Point(120, 80);
+            txtPass.Width = 150;
+            txtPass.PasswordChar = '*';
+
+            // Login Button
+            btnLogin = new Button();
+            btnLogin.Text = "Login";
+            btnLogin.Location = new Point(140, 120);
+            btnLogin.Click += BtnLogin_Click;
+
+            // Result Label
+            lblResult = new Label();
+            lblResult.Location = new Point(140, 160);
+            lblResult.ForeColor = Color.Red;
+
+            // Add controls
+            Controls.Add(txtUser);
+            Controls.Add(txtPass);
+            Controls.Add(btnLogin);
+            Controls.Add(lblResult);
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUser.Text;
+            string password = txtPass.Text;
+
+            if (username == "admin" && password == "admin123")
+            {
+                Dashboard dash = new Dashboard(username);
+                dash.Show();
+                this.Hide();
+            }
+            else
+            {
+                lblResult.Text = "Login Failed";
+            }
+        }
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace VulnThickClient1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(355, 164);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(140, 23);
+            this.btnTransfer.TabIndex = 0;
+            this.btnTransfer.Text = "Transfer Money";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTransfer);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTransfer;
+    }
+}
